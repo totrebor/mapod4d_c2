72d605781b49f94791eab50b4a5c9dfb6f7d5251
totrebor <roberto@taglioretti.it>
2022-11-05 23:46:52 +0100

    dock plugin updated

--- addons/mapod4d/mapod4d.gd
+++ addons/mapod4d/mapod4d.gd
@@ -2,11 +2,20 @@
 extends EditorPlugin
 
 var dock = null
+var metaverse_id = null
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	print("MAPOD4D plugin start")
 	dock = preload("res://addons/mapod4d/mapod4d.tscn").instantiate()
+	var button_create = dock.get_node_or_null("%ButtonCreate")
+	if button_create != null:
+		print("button_create found")
+		button_create.pressed.connect(_on_button_create_pressed)
+	metaverse_id = dock.get_node_or_null("%MetaverseID")
+	if metaverse_id != null:
+		print("metaverse_id found")
+		metaverse_id.text_changed.connect(_on_metaverse_id_text_changed)
 	## Add the loaded scene to the docks.
 	add_control_to_dock(DOCK_SLOT_LEFT_BL, dock)
 	## Note that LEFT_UL means the left of the editor, upper-left dock.
@@ -20,3 +29,19 @@ func _exit_tree():
 	remove_control_from_docks(dock)
 	# Erase the control from the memory.
 	dock.free()
+
+
+func _on_button_create_pressed():
+	pass
+
+
+func _on_metaverse_id_text_changed(new_text):
+	const allowed_characters = "[a-z0-9]"
+	var old_caret_column = metaverse_id.caret_column
+	var word = ''
+	var regex = RegEx.new()
+	regex.compile(allowed_characters)
+	for valid_character in regex.search_all(new_text):
+		word += valid_character.get_string()
+	metaverse_id.set_text(word)
+	metaverse_id.caret_column = old_caret_column
